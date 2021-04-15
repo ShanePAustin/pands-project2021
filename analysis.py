3a8cebe59b0964c47ed948cf0d6d3b9c01af0d6f
Shane Austin <shane.p.austin@gmail.com>
2021-04-15 15:59:43 +0100

    Adding shape and restructuring Summary

--- analysis.py
+++ analysis.py
@@ -16,56 +16,79 @@ df = pd.read_csv(filename, header = None, names = ["Sepal Length", "Sepal Width"
 
 ###############################################################################################################################
 #Data summary.txt
+
+shape = df.shape
+count = df.count()
+
+#count the occurances of each flower type
+countSpecies = df["Species"].value_counts()
+
+#output the first 5 lines of data per species of flower
+head = df.groupby("Species").head(5)
+
 #summary production using describe()
 summary = df.describe().round(2)
 #Summary per flower type
 sumSeto = df.loc[0:49].describe().round(2)
 sumVers = df.loc[50:99].describe().round(2)
 sumVirg = df.loc[100:149].describe().round(2)
-#count the occurances of each flower type
-count = df["Species"].value_counts()
-#output the first 5 lines of data per species of flower
-head = df.groupby("Species").head(5)
+
 #output the correlation of the 4 attributes
 correlation = df.corr()
 
+'''
 #create Summary.txt
 with open("Summary.txt", "w") as f:
     
-    f.write(("Data Summary\n\n")+(str(summary)+('\n\n')))
+    f.write(("Data Summary"\n\n)
+    
+    f.write(("Data Shape\n\n")+(str(shape)+('\n\n')))
+    f.write(("Data Count\n\n")+(str(count)+('\n\n')))    
+    f.write(("Data Count per Species\n\n")+(str(countSpecies)+('\n\n')))
+
+    f.write(("Data Head per Species\n\n")+(str(head)+('\n\n'))) 
+
+    f.write(("Data Summary \n\n")+(str(summary)+('\n\n')))
     f.write(("Data Summary (Setosa)\n\n")+(str(sumSeto)+('\n\n')))   
     f.write(("Data Summary (Versicolor)\n\n")+(str(sumVers)+('\n\n')))
     f.write(("Data Summary (Virginica)\n\n")+(str(sumVirg)+('\n\n')))
-    f.write(("Species Count\n\n")+(str(count)+('\n\n')))
-    f.write(("Data Head per Species\n\n")+(str(head)+('\n\n')))
-    f.write(("Data Correlation\n\n")+(str(correlation)))
 
-###############################################################################################################################
+    f.write(("Data Correlation\n\n")+(str(correlation)))
 '''
-sepalLength, axes = plt.subplots(figsize=(10,8))
-df["Sepal Length"].plot(kind='hist',color='firebrick')
+###############################################################################################################################
+
+ax = plt.axes()
+plt.hist(df["Sepal Length"], color = "firebrick")
 plt.title("Sepal Length Histogram")
 plt.xlabel("Sepal Length")
-plt.grid(linestyle = "dashed")
+plt.grid(linestyle = "dashed", )
+ax.set_facecolor("lightgrey")
 plt.show()
 
-sepalWidth, axes = plt.subplots(figsize=(10,8))
-df["Sepal Width"].plot(kind='hist',color='royalblue')
+ax = plt.axes()
+plt.hist(df["Sepal Width"], color = "royalblue")
+plt.title("Sepal Width Histogram")
 plt.xlabel("Sepal Width")
-plt.grid(linestyle = "dashed")
+plt.grid(linestyle = "dashed", )
+ax.set_facecolor("lightgrey")
 plt.show()
 
-petalLength, axes = plt.subplots(figsize=(10,8))
-df["Petal Length"].plot(kind='hist',color='forestgreen')
+ax = plt.axes()
+plt.hist(df["Petal Length"], color = "forestgreen")
+plt.title("Petal Length Histogram")
 plt.xlabel("Petal Length")
-plt.grid(linestyle = "dashed")
+plt.grid(linestyle = "dashed", )
+ax.set_facecolor("lightgrey")
 plt.show()
 
-petalLength, axes = plt.subplots(figsize=(10,8))
-df["Petal Width"].plot(kind='hist',color='violet')
+ax = plt.axes()
+plt.hist(df["Petal Width"], color = "violet")
+plt.title("Petal Width Histogram")
 plt.xlabel("Petal Width")
-plt.grid(linestyle = "dashed")
+plt.grid(linestyle = "dashed", )
+ax.set_facecolor("lightgrey")
 plt.show()
+'''
 
 Histogram, axes = plt.subplots(2,2, figsize=(10,10), sharex=False)
 sns.histplot( df["Sepal Length"] , color="firebrick", ax=axes[0, 0])
